1c813dd3e1fbc0efc615fa10da21385958279da2
PedroEichler <pedroheichler@gmail.com>
2026-01-06 23:41:28 -0300

    Feat: Deixando o projeto mais leve

--- FrontEnd/src/assets/musicas/album1/MusicACDC.js
+++
@@ -1,7 +0,0 @@
-import music1 from './01.mp3';
-import music2 from './02.mp3';
-import music3 from './03.mp3';
-
-const MusicACDC = [music1, music2, music3]
-
-export default MusicACDC;
--- FrontEnd/src/assets/musicas/album2/MusicOasis.js
+++
@@ -1,7 +0,0 @@
-import musica1 from './01.mp3';
-import musica2 from './02.mp3';
-import musica3 from './03.mp3';
-
-const MusicOasis = [musica1, musica2, musica3]
-
-export default MusicOasis;
